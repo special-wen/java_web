0c9601d3ac4ac666696300b17bcd72b571c82c38
special-wen <2142866269@qq.com>
2017-09-20 20:59:35 +0800

    update

--- System_manager/javascript/seat.js
+++ System_manager/javascript/seat.js
@@ -1,1 +1,31 @@
 'use strict'
+
+/*
+window.onload = function createSate() {
+   let image = document.createElement('img');
+   image.src = '../System_manager/image/1.png';
+   document.getElementById('seat').appendChild(image);
+}
+
+*/
+
+window.onload = function createSate() {
+    for(let i = 1;i<=8;i++){
+        for(let j = 1;j<=8;j++){
+            let sateImage = document.createElement('img');
+            let num = document.createElement('span');
+            sateImage.src = '../System_manager/image/1.png';
+            num.innerText = i+','+j;
+            sateImage.setAttribute("class","sate");
+            document.getElementById('seat').appendChild(sateImage);
+            document.getElementById('seat').appendChild(num);
+        }
+        let br = document.createElement('br');
+
+        document.getElementById('seat').appendChild(br);
+    }
+}
+
+//function changeSate() {
+
+//}
--- System_manager/javascript/studio.js
+++ System_manager/javascript/studio.js
@@ -1,1 +1,46 @@
 'use strict'
+
+//添加演出厅
+function addStudio() {
+    let studioAdd = document.getElementById('studio');
+
+    let name = document.getElementById('studioName').value;
+    let row = document.getElementById('sateRow').value;
+    let col = document.getElementById('sateCol').value;
+    let state = document.getElementById('studioState').value;
+    let introduction = document.getElementById('studioIntroduction').value;
+
+    let newRow = studioAdd.insertRow();
+
+    let studioName = newRow.insertCell();
+    let seatRow = newRow.insertCell();
+    let seatCell = newRow.insertCell();
+    let studioState = newRow.insertCell();
+    let studioInroduction = newRow.insertCell();
+
+    studioName.innerText = name;
+    seatRow.innerText = row;
+    seatCell.innerText = col;
+    studioState.innerText = state;
+    studioInroduction.innerText = introduction;
+
+}
+
+//选择需要删除的行,删除该行信息
+function studioRow(obj) {
+    let studio = document.getElementById('studio');
+
+    if (event.srcElement.tagName =='TD') {
+        let curRow = event.srcElement.parentElement;
+        curRow.style.background = '#888';
+
+        let deleteRow = curRow.rowIndex;
+        let deleteStudio = document.getElementById('deleteStudio');
+        deleteStudio.onclick = function () {
+            let deleteTable = document.getElementById('studio');
+            deleteTable.deleteRow(deleteRow);
+        }
+    }
+}
+
+
