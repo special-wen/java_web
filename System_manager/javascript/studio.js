e9452bceac661ffb325f9e7aa4c6fadda211d2ad
special-wen <2142866269@qq.com>
2017-09-28 16:40:42 +0800

    update

--- System_manager/javascript/studio.js
+++ System_manager/javascript/studio.js
@@ -74,11 +74,15 @@ function deleteRow(row) {
 //删除演出厅
 function removeStudio() {
     let row = number;
-    if(row == 0)
-        return 0;
-    else{
+    if(row > 0){
         console.log(row);
         deleteRow(row);
+    }else{
+        let changeButton = document.getElementById('deleteStudio');
+        changeButton.setAttribute('data-toggle', 'modal');
+        changeButton.setAttribute('data-target', '#error');
+        let text = document.getElementById('waring');
+        text.innerHTML = '请选择需要删除的地方！'
     }
 }
 
@@ -98,15 +102,112 @@ function changeRow(number) {
 
 //将信息添加在修改信息的相应位置
 function change() {
-    let change = [];
-    let array = changeRow(number);
-    change[0] = document.getElementById('changeName');
-    change[1] = document.getElementById('changeRow');
-    change[2] = document.getElementById('changeCol');
-    change[3] = document.getElementById('changeState');
-    change[4] = document.getElementById('changeInt');
-    let len = array.length;
-    for(let i = 0;i<len;i++){
-        change[i].value = array[i];
+    if(number > 0){
+        let change = [];
+        let array = changeRow(number);
+        change[0] = document.getElementById('changeName');
+        change[1] = document.getElementById('changeRow');
+        change[2] = document.getElementById('changeCol');
+        change[3] = document.getElementById('changeState');
+        change[4] = document.getElementById('changeInt');
+        let len = array.length;
+        for(let i = 0;i<len;i++){
+            change[i].value = array[i];
+        }
+    }
+    else{
+        console.log('aaa');
+        let changeButton = document.getElementById('changeStudio');
+        changeButton.setAttribute('data-toggle', 'modal');
+        changeButton.setAttribute('data-target', '#error');
+        let text = document.getElementById('waring')
+        text.innerHTML = '请选择需要修改的地方！'
+    }
+
+}
+
+function check() {
+    let aaa = document.getElementById('studioName').value;
+
+
+    console.log(aaa);
+    if(/^.{2,20}$/.test(aaa)){
+        document.getElementById('err').innerHTML = 'err:演出厅名称为2～20个字符！';
+    }
+    else{
+        document.getElementById('err').innerHTML = '';
     }
 }
+
+
+function check1() {
+    let aaa = document.getElementById('sateRow').value;
+    console.log(aaa);
+    if(/^([1-9])|(1[0-5])$/.test(aaa)){
+        document.getElementById('err1').innerHTML = 'err:演出厅中座位的行数最多为15！';
+    }else{
+        document.getElementById('err1').innerHTML = '';
+    }
+}
+
+function check2() {
+    let aaa = document.getElementById('sateCol').value;
+    console.log(aaa);
+    if(/^([1-9])|(1[0-5])$/.test(aaa)){
+        document.getElementById('err2').innerHTML = 'err:演出厅列数最多为15！';
+    }else{
+        document.getElementById('err2').innerHTML = '';
+    }
+}
+
+function check3() {
+    let aaa = document.getElementById('studioState').value;
+    console.log(aaa);
+    if(/^.{2,10}$/.test(aaa)){
+        document.getElementById('err3').innerHTML = 'err:演出厅状态最多为10个字符！';
+    }else{
+        document.getElementById('err3').innerHTML = '';
+    }
+}
+
+function check4() {
+    let aaa = document.getElementById('changeName').value;
+    console.log(aaa);
+    if(/^.{2,20}$/.test(aaa)){
+        document.getElementById('err4').innerHTML = 'err:演出厅名称为2～20个字符！';
+    }else{
+        document.getElementById('err4').innerHTML = '';
+    }
+}
+
+
+function check5() {
+    let aaa = document.getElementById('changeRow').value;
+    console.log(aaa);
+    if(/^([1-9])|(1[0-5])$/.test(aaa)){
+        document.getElementById('err5').innerHTML = 'err:演出厅中座位的行数最多为15！';
+    }else{
+        document.getElementById('err5').innerHTML = '';
+    }
+}
+
+function check6() {
+    let aaa = document.getElementById('changeCol').value;
+    console.log(aaa);
+    if(/^([1-9])|(1[0-5])$/.test(aaa)){
+        document.getElementById('err6').innerHTML = 'err:演出厅列数最多15！';
+    }else{
+        document.getElementById('err6').innerHTML = '';
+    }
+}
+
+function check7() {
+    let aaa = document.getElementById('changeState').value;
+    console.log(aaa);
+    if(/^.{2,10}$/.test(aaa)){
+        document.getElementById('err3').innerHTML = 'err:演出厅状态最多为10个字符！';
+    }else{
+        document.getElementById('err3').innerHTML = '';
+    }
+}
+
